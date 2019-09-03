bf672f756a0b4f2a70a62b0c1f5c95efde92cc28
KingAbesh <abeshekwere@gmail.com>
2019-09-03 01:55:35 +0100

    Made a few changes to prevent redundancy

--- js/volunteerng-sign-in.js
+++ js/volunteerng-sign-in.js
@@ -1,24 +1,26 @@
 //For switching different tabs.
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+const signUpButton = document.querySelector("#signUp");
+const signInButton = document.querySelector("#signIn");
+const container = document.querySelector(".container");
 
-signUpButton.addEventListener('click', () => {
-    container.classList.add("right-panel-active");
+signUpButton.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
+  shuffleWord();
 });
 
-signInButton.addEventListener('click', () => {
-    container.classList.remove("right-panel-active");
+signInButton.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
 });
 
 //Changes different words in signup tab when signup button is clicked.
-signUpButton.addEventListener('click',
-    function shuffleWord() {
-        let words = ['Enter your personal details and start your journey with us.', 'According to a report by Sarah C. and Alicia W. 67% of people found volunteer opportunities online in 2014 vs 34% in 2006.', 'Through your collaboration we can change the world one person at a time.'];
-        let wordFromArr = Math.floor(Math.random() * 3)
-        let wordFromString = words[wordFromArr];
+const shuffleWord = () => {
+  let words = [
+    "Enter your personal details and start your journey with us.",
+    "According to a report by Sarah C. and Alicia W. 67% of people found volunteer opportunities online in 2014 vs 34% in 2006.",
+    "Through your collaboration we can change the world one person at a time."
+  ];
+  const wordFromArr = Math.floor(Math.random() * 3);
+  const wordFromString = words[wordFromArr];
 
-        console.log(wordFromString);
-
-        document.getElementById("change").innerText = wordFromString;
-    });
+  document.getElementById("change").innerText = wordFromString;
+};
